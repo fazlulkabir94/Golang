ce2408b8891d9b47fd18ff67df9d48dacafb5f60
MD Shohag <fazlulkabir94@users.noreply.github.com>
2018-05-18 16:39:36 +0600

    Create switchStatement.go

---
+++ switchStatement.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt" 
+
+func main() {
+    var x int = 34;
+    switch x {
+    case 1:
+    	fmt.Println("one")
+    case 2:
+    	fmt.Println("two")
+    case 3:
+    	fmt.Println("three")
+    case 4:
+    	fmt.Println("four")
+    case 5:
+    	fmt.Println("five")
+    default:
+    	fmt.Println("nothing")
+    }
+
+}
