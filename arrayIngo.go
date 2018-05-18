749562826a190e15aabfdace6cb35baa066ce7d5
MD Shohag <fazlulkabir94@users.noreply.github.com>
2018-05-18 16:27:39 +0600

    Create arrayIngo.go

---
+++ arrayIngo.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt" 
+
+func main() {
+    ArrData := [4] string{"bangladesh", "kabir", "Shohag", "Anika"} //One dimensional array
+    fmt.Println(len(ArrData))
+    fmt.Println()
+    fmt.Println(ArrData[0])
+    data := [3][3] int{          //two dimensional array
+    	                {2,3,4},
+    	                {3,4,5},
+    	                {3,4,6},
+                    }
+    fmt.Println(data)
+    fmt.Println(data[0][0])
+    
+    //Print two dimensional array use for loop
+    for i := 0; i < 3; i++ {
+    	for j := 0; j < 3; j++ {
+    		fmt.Printf("%d ", data[i][j])
+    	}
+    	fmt.Println("");
+    }
+}
